e82cc39cb0cef13fb57502bdc7e282508ee34903
Alex Stokes <r.alex.stokes@gmail.com>
2017-08-06 16:44:52 -0700

    split out hefty computation

--- AddNOutOfGas.sol
+++ AddNOutOfGas.sol
@@ -7,12 +7,21 @@ contract AddNOutOfGas {
     n = _n;
   }
 
-  function apply(int _x) constant returns (int) {
+  function doTooMuchComputation() private constant returns (int) {
     int f = 0;
     // trigger an out-of-gas exception
     for (int i = 0; i < 200000; i++) {
       f += 1;
     }
+    return f;
+  }
+
+  function apply(int _x) constant returns (int) {
+    return _x + n;
+  }
+
+  function applyWithException(int _x) constant returns (int) {
+    doTooMuchComputation();
     return _x + n;
   }
 
